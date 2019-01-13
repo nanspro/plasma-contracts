cec2c8b3cda2b457966f3c9ba563e5548b98a882
ben-chain <ben@pseudonym.party>
2019-01-13 14:26:43 -0500

    fix linting

--- test/test-plasma.js
+++ test/test-plasma.js
@@ -31,15 +31,8 @@ for (let i = 0; i < 5; i++) {
   web3.eth.accounts.wallet.add(privateKey)
 }
 // For all provider options, see: https://github.com/trufflesuite/ganache-cli#library
-const providerOptions = {'accounts': ganacheAccounts, 'locked': false, 'logger': console}
+const providerOptions = { 'accounts': ganacheAccounts, 'locked': false, 'logger': console }
 web3.setProvider(ganache.provider(providerOptions))
-
-async function compileVyper (path) {
-  const bytecodeOutput = await exec('vyper ' + path + ' -f bytecode')
-  const abiOutput = await exec('vyper ' + path + ' -f abi')
-  // Return both of the output's stdout without the last character which is \n
-  return [bytecodeOutput.stdout.slice(0, -1), abiOutput.stdout.slice(0, -1)]
-}
 
 async function mineBlock () {
   return new Promise((resolve, reject) => {
@@ -105,7 +98,7 @@ describe('Plasma', () => {
     [bytecode, abi] = await compilePlasmaContract()
     const addr = web3.eth.accounts.wallet[0].address
 
-    plasmaCt = new web3.eth.Contract(JSON.parse(abi), addr, {from: addr, gas: 3500000, gasPrice: '300000'})
+    plasmaCt = new web3.eth.Contract(JSON.parse(abi), addr, { from: addr, gas: 3500000, gasPrice: '300000' })
     // const balance = await web3.eth.getBalance(accounts[0].address)
     const bn = await web3.eth.getBlockNumber()
     await mineBlock()
@@ -123,8 +116,8 @@ describe('Plasma', () => {
     const deploymentTransaction = await web3.eth.getTransaction(block.transactions[0]) // eslint-disable-line no-unused-vars
     freshContractSnapshot = await getCurrentChainSnapshot()
 
-  txs = getSequentialTxs(32)
-  tree = new PlasmaMerkleSumTree(txs)
+    txs = getSequentialTxs(32)
+    tree = new PlasmaMerkleSumTree(txs)
   })
 
   it('Should compile the vyper contract without errors', async () => {
@@ -138,7 +131,7 @@ describe('Plasma', () => {
   const dummyBlockHash = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
   it('should allow a block to be published by the operator', async () => {
     await mineNBlocks(10) // blocktime is 10
-    await plasma.methods.submitBlock(dummyBlockHash).send({value: 0, from: web3.eth.accounts.wallet[0].address, gas: 4000000}, async function (error, result) { // get callback from function which is your transaction key
+    await plasma.methods.submitBlock(dummyBlockHash).send({ value: 0, from: web3.eth.accounts.wallet[0].address, gas: 4000000 }, async function (error, result) { // get callback from function which is your transaction key
       if (!error) {
         // const receipt = await web3.eth.getTransactionReceipt(result)
       } else {
@@ -151,9 +144,9 @@ describe('Plasma', () => {
   it('should allow a first deposit and add it to the deposits correctly', async () => {
     let depositEnd, depositNextStart
     const depositSize = 50
-    await plasma.methods.deposit(0).send({value: depositSize, from: web3.eth.accounts.wallet[1].address, gas: 4000000}, async function (error, result) { //get callback from function which is your transaction key
+    await plasma.methods.deposit(0).send({ value: depositSize, from: web3.eth.accounts.wallet[1].address, gas: 4000000 }, async function (error, result) { // get callback from function which is your transaction key
       if (error) {
-        assert.equal(true, false) // theres a better way but need to fail tests when things throw
+        assert.equal(true, false) // there'ss a better way but need to fail tests when things throw
         console.log(error)
       }
     }).catch((error) => { console.log('send callback failed: ', error) })
@@ -164,15 +157,15 @@ describe('Plasma', () => {
     bigDepositSnapshot = getCurrentChainSnapshot()
   })
   it('should allow left, right, and un-aligned exits if unchallenged', async () => {
-    await plasma.methods.beginExit(0, 0, 10, 0).send({value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000})
-    await plasma.methods.beginExit(0, 20, 30, 0).send({value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000})
-    await plasma.methods.beginExit(0, 40, 50, 0).send({value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000})
+    await plasma.methods.beginExit(0, 0, 10, 0).send({ value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000 })
+    await plasma.methods.beginExit(0, 20, 30, 0).send({ value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000 })
+    await plasma.methods.beginExit(0, 40, 50, 0).send({ value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000 })
 
     await mineNBlocks(20)
 
-    await plasma.methods.finalizeExit(0, '0x' + IMAGINARY_PRECEDING.toString(16)).send({value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000})
-    await plasma.methods.finalizeExit(1, 0).send({value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000})
-    await plasma.methods.finalizeExit(2, 10).send({value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000})
+    await plasma.methods.finalizeExit(0, '0x' + IMAGINARY_PRECEDING.toString(16)).send({ value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000 })
+    await plasma.methods.finalizeExit(1, 0).send({ value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000 })
+    await plasma.methods.finalizeExit(2, 10).send({ value: 0, from: web3.eth.accounts.wallet[1].address, gas: 4000000 })
 
     const imaginaryNext = await plasma.methods.depositedRanges__nextDepositStart('0x' + IMAGINARY_PRECEDING.toString(16)).call()
     const firstDepositEnd = await plasma.methods.depositedRanges__end(0).call()
@@ -191,9 +184,9 @@ describe('Plasma', () => {
     assert.equal(lastDepositNextStart, MAX_END.toString())
   })
   it('should allow re-deposits into exited ranges', async () => {
-    await plasma.methods.deposit(0).send({value: 5, from: web3.eth.accounts.wallet[1].address, gas: 4000000})
-    await plasma.methods.deposit(10).send({value: 10, from: web3.eth.accounts.wallet[1].address, gas: 4000000})
-    await plasma.methods.deposit(10).send({value: 5, from: web3.eth.accounts.wallet[1].address, gas: 4000000})
+    await plasma.methods.deposit(0).send({ value: 5, from: web3.eth.accounts.wallet[1].address, gas: 4000000 })
+    await plasma.methods.deposit(10).send({ value: 10, from: web3.eth.accounts.wallet[1].address, gas: 4000000 })
+    await plasma.methods.deposit(10).send({ value: 5, from: web3.eth.accounts.wallet[1].address, gas: 4000000 })
 
     const firstRangeEnd = await plasma.methods.depositedRanges__end(0).call()
     const firstRangeNext = await plasma.methods.depositedRanges__nextDepositStart(0).call()
@@ -202,7 +195,7 @@ describe('Plasma', () => {
     assert.equal(firstRangeEnd, '5')
     assert.equal(firstRangeNext, '10')
     assert.equal(middleRangeEnd, '45')
-    assert.equal(middleRangeNext, '170141183460469231731687303715884105727')  
+    assert.equal(middleRangeNext, '170141183460469231731687303715884105727')
   })
   it('should getLeafHash of an encoded transaction', async () => {
     const index = Math.floor(Math.random() * 32)
@@ -211,14 +204,13 @@ describe('Plasma', () => {
     assert.equal(possibleHash, '0x' + tree.levels[0][index].hash)
   })
   it('should decodeBlockNumber from a tx', async () => {
-    //todo make this not 0 to improve testing coverage lol
+    // todo make this not 0 to improve testing coverage lol
     const index = Math.floor(Math.random() * 32)
     const tx = txs[index]
     const possibleBN = await plasma.methods.decodeBlockNumber('0x' + tx.encoded).call()
     assert.equal(possibleBN, new BN(tx.args.transfer.block).toString())
   })
   it('should decode transferBounds from a tx', async () => {
-    //todo make this not 0 to improve testing coverage lol
     const index = Math.floor(Math.random() * 32)
     const tx = txs[index]
     const possibleBounds = await plasma.methods.decodeIthTransferBounds(0, '0x' + tx.encoded).call()
@@ -226,36 +218,55 @@ describe('Plasma', () => {
     assert.equal(possibleBounds[1], new BN(tx.args.transfer.end).toString())
   })
   it('should decode transferFrom from a tx', async () => {
-    //todo make this not 0 to improve testing coverage lol
+    // todo make this not 0 address to improve testing coverage
     const index = Math.floor(Math.random() * 32)
     const tx = txs[index]
     const possibleFrom = await plasma.methods.decodeIthTransferFrom(0, '0x' + tx.encoded).call()
     assert.equal(possibleFrom, tx.args.transfer.sender)
   })
   it('should decode transferTo from a tx', async () => {
-    //todo make this not 0 to improve testing coverage lol
+    // todo make this not 0 address to improve testing coverage
     const index = Math.floor(Math.random() * 32)
     const tx = txs[index]
     const possibleFrom = await plasma.methods.decodeIthTransferTo(0, '0x' + tx.encoded).call()
     assert.equal(possibleFrom, tx.args.transfer.recipient)
   })
-  it('should well-decode transactions and verify their proofs', async () => {
-    debugger
-    await plasma.methods.submitBlock('0x' + tree.root().hash).send({value: 0, from: web3.eth.accounts.wallet[0].address, gas: 4000000})
+  it('should properly check individual branch proofs and get implicit bounds', async () => {
+    await plasma.methods.submitBlock('0x' + tree.root().hash).send({ value: 0, from: web3.eth.accounts.wallet[0].address, gas: 4000000 })
     const index = Math.floor(Math.random() * 32)
     let proof = tree.getInclusionProof(index)
     const parsedSum = proof[0].sum
     proof.shift()
     let proofString = '0x'
     proof.forEach((element) => { proofString = proofString + element.hash + element.sum.toString(16, 32) })
-    const shouldBeRoot = await plasma.methods.checkBranchAndGetBounds(
+    const possibleImplicitBounds = await plasma.methods.checkBranchAndGetBounds(
       web3.utils.soliditySha3('0x' + tree.leaves[index].encoded),
       '0x' + parsedSum.toString(16, 32),
       index,
       proofString,
       1
     ).call()
+    assert.equal(possibleImplicitBounds[0], new BN(txs[index].args.transfer.start))
+    assert(new BN(possibleImplicitBounds[1]).gte(new BN(txs[index].args.transfer.end)))
+  })
+  it('should properly check full tx proofs and get transfer & blocknumber', async () => {
+    const index = Math.floor(Math.random() * 32)
+    let proof = tree.getInclusionProof(index)
+    const parsedSum = proof[0].sum
+    proof.shift()
+    let proofString = '0x'
+    proof.forEach((element) => { proofString = proofString + element.hash + element.sum.toString(16, 32) })
     debugger
+    const possibleImplicitBounds = await plasma.methods.checkTXValidityAndGetTransfer(
+      0,
+      web3.utils.soliditySha3('0x' + tree.leaves[index].encoded),
+      '0x' + parsedSum.toString(16, 32),
+      '0x' + new BN(index).toString(16, 8),
+      proofString
+    ).call()
+    debugger
+    assert.equal(possibleImplicitBounds[0], new BN(txs[index].args.transfer.start))
+    assert(new BN(possibleImplicitBounds[1]).gte(new BN(txs[index].args.transfer.end)))
   })
 })
 
@@ -273,9 +284,9 @@ const getSequentialTxs = (n) => {
         sender: '0x0000000000000000000000000000000000000000',
         recipient: '0x0000000000000000000000000000000000000000',
         token: 0,
-        start: i * 10,
-        end: (i + 1) * 10,
-        block: 0
+        start: i * 20,
+        end: (i + 0.5) * 20,
+        block: 1
       },
       signature: {
         v: 0,
