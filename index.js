0b4c2dac14a546878899906337b9a8d0f25cc7b5
ben-chain <ben@pseudonym.party>
2019-01-19 14:39:12 -0500

    chainge chain contract name

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
- async function compileVyper (path) {
+async function compileVyper (path) {
   const bytecodeOutput = await exec('vyper ' + path + ' -f bytecode')
   const abiOutput = await exec('vyper ' + path + ' -f abi')
   // Return both of the output's stdout without the last character which is \n
   return [ bytecodeOutput.stdout.slice(0, -1), abiOutput.stdout.slice(0, -1) ]
 }
 
- async function compilePlasmaContract () {
-  return compileVyper('./contracts/plasmaprime.vy')
+async function compilePlasmaContract () {
+  return compileVyper('./contracts/PlasmaChain.vy')
 }
 
- module.exports = compilePlasmaContract
+module.exports = compilePlasmaContract
