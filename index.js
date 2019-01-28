06574be92632947550eeee482355c87e3d00550a
Kelvin Fichter <kelvinfichter@gmail.com>
2019-01-28 13:17:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const plasmaChainCompiled = require('./compiled-contracts/plasma-chain.js')
 const plasmaRegistryCompiled = require('./compiled-contracts/plasma-registry.js')
+const erc20Compiled = require('./compiled-contracts/test-token.js')
 const utils = require('./utils.js')
 
 module.exports = {
   plasmaChainCompiled,
   plasmaRegistryCompiled,
+  erc20Compiled,
   utils
 }
